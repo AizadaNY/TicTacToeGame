907855f4ccc9d92d658d8ca7113dc70383ece3d4
AizadaNY <aizada.abdyrakhmanova@gmail.com>
2024-01-04 23:09:09 -0500

    set

---
+++ src/main/java/model/SmartPlayer.java
@@ -0,0 +1,60 @@
+package model;
+
+import java.util.Random;
+
+public class SmartPlayer {
+
+    private String name;
+    private String sign;
+
+    public SmartPlayer(String name,String sign) {
+        this.name=name;
+        this.sign=sign;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSign() {
+        return sign;
+    }
+
+    public void setSign(String sign) {
+        this.sign = sign;
+    }
+
+
+    public int getRowNumber(int bound){
+        int row=
+
+        return row;
+    }
+
+    public int getColumnNumber(int bound){
+        int column=
+
+        return column;
+    }
+
+    //block user1
+    //collect 3
+    public void blockUser(){
+        //get user move
+        //move to next cell
+    }
+
+    public void checkUserElements(){
+        //check what users made
+    }
+
+
+
+
+
+
+}
