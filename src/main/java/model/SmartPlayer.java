8bb8ad9a6b025dcb6ed596c5c87b78c8b072afb1
AizadaNY <aizada.abdyrakhmanova@gmail.com>
2024-01-16 16:10:31 -0500

    smart player update"

--- src/main/java/model/SmartPlayer.java
+++ src/main/java/model/SmartPlayer.java
@@ -9,6 +9,7 @@ public class SmartPlayer extends Player {
         super(name, sign);
     }
 
+
     public int getRowNumber(int bound) {
         int row = 0;
 
@@ -21,129 +22,84 @@ public class SmartPlayer extends Player {
         return column;
     }
 
-    //block user1
-    //collect 3
-    public void blockUser() {
-        //get user move
-        //move to next cell
-    }
-
-
-    int blankRow = 0;
-    int blankColumn = 0;
-    int rowCount = 0;
-    int columnCount = 0;
-    int diagonal1Count = 0;
-    int diagonal2Count = 0;
-    int myRowCount = 0;
-
-    public void checkUserElements(String[][] table, int row, int column, String competitorSign) {
-        for (int i = 0; i < table.length; i++) {
-            //check row
-            if (table[row][i] == competitorSign) {
-                rowCount++;
-            } else if (table[row][i] == getSign()) {
-                myRowCount++;
-            } else {
-                blankColumn = i;
-            }
-            //column
-            if (table[i][column] == competitorSign) {
-                columnCount++;
-            } else {
-                blankColumn = column;
-                blankRow = i;
-            }
-            if (row == column) {
-                //diagonal
-                if (table[i][i] == competitorSign) {
-                    diagonal1Count++;
-                } else {
-                    blankColumn = i;
-                    blankRow = i;
-                }
-
-            } else if (row + column == table.length - 1) {
-                int k = table.length - 1;
-                k--;
-                if (table[k][i] == competitorSign) {
-                    diagonal2Count++;
-                } else {
-                    blankColumn = i;
-                    blankRow = i;
-                }
-            }
-
-        }
-
-
-    }
-
-    public int checkRow(String[][] table, int row, String competitorSign, int i) {
-        int count = 0;
-
-        if (table[row][i] == competitorSign) {
-            count++;
-        } else {
-            blankRow = row;
-            blankColumn = i;
-        }
-
-        return count;
-    }
-
-
-    public int checkColumn(String[][] table, int column, String competitorSign, int i) {
-        int count = 0;
-
-        if (table[i][column] == competitorSign) {
-            count++;
-        } else {
-            blankColumn = column;
-            blankRow = i;
-        }
-        return count;
-    }
-
-    public int checkDiagonal(String[][] table, int row, int column, String competitorSign, int i) {
-        int count = 0;
-
-        if (table[i][i] == competitorSign) {
-            count++;
-        } else {
-            blankColumn = i;
-            blankRow = i;
-        }
-
-        return count;
-    }
-
 
     public void checkNeighbourCells(String[][] table, int row, int column, String competitorSign){
-
-
+        int rowToMove=0;
+        int columnToMove=0;
+        int bottomRow=row-1;
+        int topRow=row+1;
+        int rightColumn=row-1;
+        int leftColumn=row+1;
+        String bottomCell;
+        String topCell;
+        String rightCell;
+        String leftCell;
          if(row>0&&row<table.length-1){
-             String neighCellValue1=table[row-1][column];
-             String neighCellValue2=table[row+1][column];
-             if(column>0&&column< table.length-1){
-                 String neighCellValue3=table[row][column-1];
-                 String neighCellValue4=table[row][column+1];
-             }else if(column==0){
-                 String neighCellValue3=table[row][column+1];
-             }else{
-                 String neighCellValue3=table[row][column-1];
+             bottomCell=table[bottomRow][column];
+             topCell=table[topRow][column];
+             //check sign-----------------------------------
+             if(topCell==competitorSign) {
+                 if(bottomCell==null||bottomCell.isEmpty()||bottomCell==null||bottomCell==null) {
+                     rowToMove = bottomRow;
+                     columnToMove = column;
+                 }
+             }else if(bottomCell==competitorSign){
+                 if(topCell==null||topCell.isEmpty()){
+                     rowToMove=topRow;
+                     rowToMove=column;
+                 }
              }
-         } else if(row==0){
-             String neighCellValue1=table[row+1][column];
+             //----  ------------------------------
              if(column>0&&column< table.length-1){
-                 String neighCellValue2=table[row][column-1];
-                 String neighCellValue3=table[row][column+1];
+                  leftCell=table[row][leftColumn];
+                  rightCell=table[row][rightColumn];
+
+                  if(leftCell==competitorSign) {
+                      if (rightCell == null || rightCell.isEmpty()) {
+                          rowToMove = row;
+                          columnToMove = rightColumn;
+                      }
+                  }
+                 if(rightCell==competitorSign) {
+                     if (leftCell == null || leftCell.isEmpty()) {
+                         rowToMove = row;
+                         columnToMove = leftColumn;
+                     }
+                 }
+                 if(row==column){
+                     //dopgRow
+                 }
+
              }else if(column==0){
-                 String neighCellValue2=table[row][column+1];
-                 String neighCellValue3=table[row+1][column+1];
+                  rightCell=table[row][rightColumn];
+                  if(rightCell==competitorSign){
+                      if(table[row][rightColumn+1].isEmpty()||table[row][rightColumn+1]==null){
+                          rowToMove=row;
+                          columnToMove=rightColumn+1;
+                      }
+                  }
              }else{
-                 String neighCellValue2=table[row][column-1];
-                 String neighCellValue3=table[row+1][column-1];
+                  leftCell=table[row][leftColumn];
+                  if(leftCell==competitorSign){
+                      if(table[row][leftColumn-1].isEmpty()||table[row][leftColumn-1]==null){
+                          rowToMove=row;
+                          columnToMove=leftColumn-1;
+                      }
+                  }
+             }
+
+
+         } else if(row==0){
+                  bottomCell=table[bottomRow][column];
+             if(column>0&&column< table.length-1){
+                 String neighCellValue2=table[row][leftColumn];
+                 String neighCellValue3=table[row][rightColumn];
+             }else if(column==0){
+                 String neighCellValue2=table[row][rightColumn];
+                 String neighCellValue3=table[topRow][leftColumn];
+             }else{
+                 String neighCellValue2=table[row][leftColumn];
+                 String neighCellValue3=table[topRow][leftColumn];
              }
          }else{
              String neighCellValue1=table[row-1][column];
@@ -158,10 +114,9 @@ public class SmartPlayer extends Player {
                  String neighCellValue3=table[row-1][column-1];
              }
          }
-
-
-
     }
+
+
 
 
 
